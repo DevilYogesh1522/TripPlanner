a20ead9501003c52bd407309de1bf6229b41f6e0
DevilYogesh1522 <133919650+DevilYogesh1522@users.noreply.github.com>
2025-07-29 12:02:29 +0530

    firebase Config

--- src/service/FirebaseConfig.jsx
+++ src/service/FirebaseConfig.jsx
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBY_kPb2v9j-PstWMEbCLvoXt4gX21kcaY",
+  apiKey:import.meta.env.VITE_Firebase_key,
   authDomain: "tripplanner-1bf50.firebaseapp.com",
   projectId: "tripplanner-1bf50",
   storageBucket: "tripplanner-1bf50.firebasestorage.app",
@@ -17,5 +18,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+// Initialize Firestore
+export const db=getFirestore(app);      
+
+// const analytics = getAnalytics(app);
