6ab93bec9abc79c64ad18fbb1817ce2e9979f8e0
DevilYogesh1522 <133919650+DevilYogesh1522@users.noreply.github.com>
2025-08-05 19:29:06 +0530

    Refactor App component layout and improve styling with gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/custom/Header";
 import { Footer } from "./components/custom/Footer";
-import { ToastContainer } from "react-toastify";
 import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-gradient-to-br from-blue-50 to-purple-50">
       <Header />
-      
+
+      {/* Main content grows to push footer down */}
+      <main className="flex-grow">
         <Outlet />
-    
+      </main>
+
       <Footer />
-     <Toaster/>
+      <Toaster />
     </div>
   );
 }
 
 export default App;
-
-
-
